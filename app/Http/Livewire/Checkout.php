b5e767b70c8fdf32ab3e3b267e32cbf0356fd074
marininik <m.nikolli@lswr.it>
2022-06-28 16:41:22 +0200

    checkout with validation

--- app/Http/Livewire/Checkout.php
+++ app/Http/Livewire/Checkout.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Livewire;
 
-use App\Cart\Contracts\CartInterface;
-use App\Models\ShippingType;
+// use money;
 use Livewire\Component;
+use App\Models\ShippingType;
+use App\Cart\Contracts\CartInterface;
 
 class Checkout extends Component
 {
@@ -12,16 +13,65 @@ class Checkout extends Component
 
     public $shippingTypes;
 
+    public $accountForm = [
+        'email' => ''
+    ];
+
+    public $shippingForm = [
+        'address' => '',
+        'city' => '',
+        'postcode' => ''
+    ];
+
+    protected $validationAttributes = [
+        'accountForm.email' => 'email address',
+        'accountForm.address' => 'address',
+        'accountForm.city' => 'city',
+    ];
+
+    protected $messages = [
+        'accountForm.email.unique' => ' Duket se regjistruar nje here. Me mire logohu '
+    ];
+
+    public function rules()
+    {
+        return [
+            'accountForm.email' => 'required|email|max:255|unique:users,email' . (auth()->user() ? ',' . auth()->user()->id : '' ),
+            'accountForm.address' => 'required | max:255',
+            'accountForm.city' => 'required | max:255',
+            'accountForm.postcode' => 'required | max:255',
+            'shippingId' => 'required |exists:shipping_types,id',
+        ];
+    }
+
+    public function checkout()
+    {
+        $this->validate();
+
+        dd('created');
+
+    }
+
     public function mount()
     {
         $this->shippingTypes = ShippingType::orderBy('price', 'asc')->get();
 
         $this->shippingId = $this->shippingTypes->first()->id;
+
+        if ($user = auth()->user()) {
+            // dd($user);
+            $this->accountForm['email'] = $user->email;
+        }
     }
 
     public function getShippingTypeProperty()
     {
         return $this->shippingTypes->find($this->shippingId);
+    }
+
+    public function getTotalProperty(CartInterface $cart)
+    {
+        return $cart->subtotal() + $this->shippingType->price;
     }
 
     public function render(CartInterface $cart)
--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -1,12 +1,21 @@
-<form>
+<form wire:submit.prevent='checkout'>
     <div class="grid grid-flow-col grid-cols-6 gap-4 overflow-hidden sm:rounded-lg">
         <div class="self-start col-span-3 p-6 space-y-6 bg-white border-b border-gray-200">
+
+            @guest
             <div class="space-y-3">
                 <div class="text-lg font-semibold"> Account details </div>
+                {{ print_r( $accountForm )}}
+                <label for="email">Email</label>
+                <x-input id="email" class="block w-full mt-1" type="text" name="email" wire:model.defer='accountForm.email' />
+                @error('accountForm.email')
+                <div class="mt-2 font-semibold text-red-500">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
-            <label for="email">Email</label>
-            <x-input id="email" class="block w-full mt-1" type="text" name="email" />
-            <div class="mt-2 font-semibold text-red-500">An error occured</div>
+            @endguest
+
 
             <div class="space-y-3">
                 <div class="text-lg font-semibold"> Shipping </div>
@@ -16,39 +25,50 @@
                 </x-select>
             </div>
 
-
             <div class="space-y-3">
                 <div>
                     <label for="address">Address</label>
-                    <x-input id="address" class="block w-full mt-1" type="text" name="address" />
-                    <div class="mt-2 font-semibold text-red-500">An error occured</div>
+                    <x-input id="address" class="block w-full mt-1" type="text" name="address" wire:model.defer='accountForm.address' />
+                    @error('accountForm.address')
+                    <div class="mt-2 font-semibold text-red-500">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                    {{-- <div class="mt-2 font-semibold text-red-500">An error occured</div> --}}
                 </div>
             </div>
 
-
             <div class="grid grid-cols-2 gap-4">
                 <div class="col-span-1">
-                    <label for="address">City</label>
-                    <x-input id="address" class="block w-full mt-1" type="text" name="address" />
-                    <div class="mt-2 font-semibold text-red-500">An error occured</div>
+                    <label for="city">City</label>
+                    <x-input id="city" class="block w-full mt-1" type="text" wire:model.defer='accountForm.city' />
+                    @error('accountForm.city')
+                    <div class="mt-2 font-semibold text-red-500">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                    {{-- <div class="mt-2 font-semibold text-red-500">An error occured</div> --}}
                 </div>
                 <div class="col-span-1">
-                    <label for="address">Postal</label>
-                    <x-input id="address" class="block w-full mt-1" type="text" name="address" />
-                    <div class="mt-2 font-semibold text-red-500">An error occured</div>
+                    <label for="postal">Postal</label>
+                    <x-input id="address" class="block w-full mt-1" type="text" wire:model.defer='accountForm.postcode' />
+                    @error('accountForm.postcode')
+                    <div class="mt-2 font-semibold text-red-500">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                    {{-- <div class="mt-2 font-semibold text-red-500">An error occured</div> --}}
                 </div>
             </div>
 
             <div class="space-y-3">
                 <div class="font-semibold text-lg">Delivery</div>
 
-                {{-- {{ $shippingId }} --}}
                 <div class="space-y-1">
                     <x-select class="w-full" wire:model='shippingId'>
                         @foreach ($shippingTypes as $shipping)
                             <option value="{{ $shipping->id }}">{{ $shipping->title }} ({{ $shipping->formattedPrice() }})</option>
                         @endforeach
-                        {{-- <option>Shipping type ($0)</option> --}}
                     </x-select>
                 </div>
             </div>
@@ -87,8 +107,8 @@
                 @endforeach
             </div>
             <div class="space-y-2 flex items-center justify-between">
-                <div class="font-bold">Total </div>
-                <h1 class="font-semibold">$0</h1>
+                <div class="font-bold">Subtotal </div>
+                <h1 class="font-semibold">{{ $cart->formattedSubtotal() }}</h1>
             </div>
             <div class="space-y-1 flex items-center justify-between">
                 <div class="font-bold">Shipping ({{ $this->shippingType->title }}) </div>
@@ -97,8 +117,9 @@
             </div>
             <div class="space-y-1 flex items-center justify-between">
                 <div class="font-bold">Total </div>
-                <h1 class="font-semibold">$0</h1>
+                <h1 class="font-semibold">{{ money($this->total) }}</h1>
             </div>
+            <x-button type="submit">Confirm order</x-button>
         </div>
     </div>
 </form>
