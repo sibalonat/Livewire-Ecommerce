4fea05939b6bb323f723c7d0d1f00efcdec2403b
marininik <m.nikolli@lswr.it>
2022-06-28 12:11:18 +0200

    .

--- app/Http/Livewire/Checkout.php
+++ app/Http/Livewire/Checkout.php
@@ -3,14 +3,32 @@
 namespace App\Http\Livewire;
 
 use App\Cart\Contracts\CartInterface;
+use App\Models\ShippingType;
 use Livewire\Component;
 
 class Checkout extends Component
 {
+    public $shippingId;
+
+    public $shippingTypes;
+
+    public function mount()
+    {
+        $this->shippingTypes = ShippingType::orderBy('price', 'asc')->get();
+
+        $this->shippingId = $this->shippingTypes->first()->id;
+    }
+
+    public function getShippingTypeProperty()
+    {
+        return $this->shippingTypes->find($this->shippingId);
+    }
+
     public function render(CartInterface $cart)
     {
         return view('livewire.checkout', [
-            'cart' => $cart
+            'cart' => $cart,
+            // 'shippingTypes' => ShippingType::orderBy('price', 'asc')->get(),
         ]);
     }
 }
--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -1,4 +1,4 @@
-<form action="">
+<form>
     <div class="grid grid-flow-col grid-cols-6 gap-4 overflow-hidden sm:rounded-lg">
         <div class="self-start col-span-3 p-6 space-y-6 bg-white border-b border-gray-200">
             <div class="space-y-3">
@@ -8,32 +8,54 @@
             <x-input id="email" class="block w-full mt-1" type="text" name="email" />
             <div class="mt-2 font-semibold text-red-500">An error occured</div>
 
-        </div>
-    </div>
-    <div class="space-y-3">
-        <div class="text-lg font-semibold"> Shipping </div>
-        <x-select class="w-full">
-            <option value="">Chose pre-saved address</option>
-            <option value="">Chose pre-saved address</option>
-        </x-select>
-
-        <div class="space-y-3">
-            <div>
-                <label for="address">Address</label>
-                <x-input id="address" class="block w-full mt-1" type="text" name="address" />
-                <div class="mt-2 font-semibold text-red-500">An error occured</div>
+            <div class="space-y-3">
+                <div class="text-lg font-semibold"> Shipping </div>
+                <x-select class="w-full">
+                    <option value="">Chose pre-saved address</option>
+                    <option value="">Chose pre-saved address</option>
+                </x-select>
             </div>
+
+
+            <div class="space-y-3">
+                <div>
+                    <label for="address">Address</label>
+                    <x-input id="address" class="block w-full mt-1" type="text" name="address" />
+                    <div class="mt-2 font-semibold text-red-500">An error occured</div>
+                </div>
+            </div>
+
+
             <div class="grid grid-cols-2 gap-4">
                 <div class="col-span-1">
                     <label for="address">City</label>
                     <x-input id="address" class="block w-full mt-1" type="text" name="address" />
                     <div class="mt-2 font-semibold text-red-500">An error occured</div>
                 </div>
+                <div class="col-span-1">
+                    <label for="address">Postal</label>
+                    <x-input id="address" class="block w-full mt-1" type="text" name="address" />
+                    <div class="mt-2 font-semibold text-red-500">An error occured</div>
+                </div>
+            </div>
+
+            <div class="space-y-3">
+                <div class="font-semibold text-lg">Delivery</div>
+
+                {{-- {{ $shippingId }} --}}
+                <div class="space-y-1">
+                    <x-select class="w-full" wire:model='shippingId'>
+                        @foreach ($shippingTypes as $shipping)
+                            <option value="{{ $shipping->id }}">{{ $shipping->title }} ({{ $shipping->formattedPrice() }})</option>
+                        @endforeach
+                        {{-- <option>Shipping type ($0)</option> --}}
+                    </x-select>
+                </div>
             </div>
         </div>
         <div class="self-start col-span-3 p-6 space-y-4 bg-white border-b border-gray-200">
             <div>
-                @foreach ($cart->contents() as $product)
+                @foreach ($cart->contents() as $variation)
                     <div class="flex items-start py-3 border-b">
                         <div class="w-16 mr-4">
                             <img src="{{ $variation->getFirstMediaUrl('default', 'thumb200x200') }}" class="w-16"
@@ -60,8 +82,22 @@
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 @endforeach
+            </div>
+            <div class="space-y-2 flex items-center justify-between">
+                <div class="font-bold">Total </div>
+                <h1 class="font-semibold">$0</h1>
+            </div>
+            <div class="space-y-1 flex items-center justify-between">
+                <div class="font-bold">Shipping ({{ $this->shippingType->title }}) </div>
+                <h1 class="font-semibold">{{ $this->shippingType->formattedPrice() }}</h1>
+
+            </div>
+            <div class="space-y-1 flex items-center justify-between">
+                <div class="font-bold">Total </div>
+                <h1 class="font-semibold">$0</h1>
             </div>
         </div>
     </div>
