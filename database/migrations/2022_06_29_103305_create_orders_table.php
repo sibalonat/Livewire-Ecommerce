5b0607da49ee8a9d2648c7e73417278886be2aec
marininik <m.nikolli@lswr.it>
2022-06-29 12:39:19 +0200

    lane order steps

---
+++ database/migrations/2022_06_29_103305_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\ShippingAddress;
+use App\Models\ShippingType;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('email');
+            $table->foreignIdFor(User::class)->nullable()->constrained();
+            $table->foreignIdFor(ShippingAddress::class)->constrained();
+            $table->foreignIdFor(ShippingType::class)->constrained();
+            $table->integer('subtotal');
+            $table->timestamp('placed_at');
+            $table->timestamp('shipped_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
