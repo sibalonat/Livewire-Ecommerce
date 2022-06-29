b1030fcd6728ce9860a0d9fd6b27d935a23285db
marininik <m.nikolli@lswr.it>
2022-06-29 12:48:49 +0200

    .

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,6 +10,14 @@ class Order extends Model
 {
     use HasFactory;
 
+    public $fillable = [
+        'email',
+        'subtotal',
+        'placed_at',
+        'packaged_at',
+        'shipped_at',
+    ];
+
     public $timestamps = [
         'placed_at',
         'packaged_at',
